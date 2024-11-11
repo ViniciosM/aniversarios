5df7caf8cdc4b875fd690b1389c6203d994dba7f
vinirod <vinicioswork@gmail.com>
2024-11-11 20:44:54 -0300

    fix string

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,8 +9,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   providers: [Google],
   adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    url: process.env.SUPABASE_URL ?? '',
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
   }),
   callbacks: {
     async session({ session, user }) {
