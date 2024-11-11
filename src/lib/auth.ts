20e3a29edc7ae6a33418f6021dff18b776dd562f
vinirod <vinicioswork@gmail.com>
2024-11-11 09:00:53 -0300

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,10 +9,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   providers: [Google],
   adapter: SupabaseAdapter({
-    // url: process.env.SUPABASE_URL,
-    // secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
-    url: "https://wxwdzmimfahgemtgksay.supabase.co",
-    secret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4d2R6bWltZmFoZ2VtdGdrc2F5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTE2NjYyOSwiZXhwIjoyMDQ2NzQyNjI5fQ.NVkFpz6QFDJhvmh2lBJt4ynp7n5We_UHkkH3lhn9tmE'
+    url: process.env.SUPABASE_URL,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
   }),
   callbacks: {
     async session({ session, user }) {
