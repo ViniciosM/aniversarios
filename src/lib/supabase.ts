38cef157bd59d1cc039e71e616209fbbbbdd9def
vinirod <vinicioswork@gmail.com>
2024-11-12 11:09:16 -0300

    configurando authjs + supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
+//import { useSession } from 'next-auth/react'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
+//const { supabaseAccessToken } = useSession
 
 export const supabase = createClient(supabaseUrl ?? "", supabaseAnonKey ?? "");
+  //{
+  // global: {
+  //   headers: {
+  //     Authorization: `Bearer ${supabaseAccessToken}`,
+  //   },
+  // },
+//}
+//);
+
